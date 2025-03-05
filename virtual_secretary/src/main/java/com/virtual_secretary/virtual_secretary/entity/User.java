80bad94eb866a6134d99f65d318328c68faf4725
tuyen-ngduc <nguyenductuyen1202@gmail.com>
2025-03-05 16:59:33 +0700

    update

--- virtual_secretary/src/main/java/com/virtual_secretary/virtual_secretary/entity/User.java
+++ virtual_secretary/src/main/java/com/virtual_secretary/virtual_secretary/entity/User.java
@@ -1,8 +1,36 @@
 package com.virtual_secretary.virtual_secretary.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String username;
+    String password;
+    String name;
+    LocalDate dob;
+    String phoneNumber;
+    String degree;
+    String identification;
+    String address;
+    String bankAccount;
+    String bank;
+    String email;
 }
